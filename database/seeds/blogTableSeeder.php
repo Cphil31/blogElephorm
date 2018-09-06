2c38b866a6ddfbafcb2ee8e6da25b0f2439ef2fb
Cphil31 <cecilephilippe31@gmail.com>
2018-09-06 15:41:35 +0200

    creation de dates aléatoires

--- database/seeds/blogTableSeeder.php
+++ database/seeds/blogTableSeeder.php
@@ -1,9 +1,15 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class blogTableSeeder extends Seeder
 {
+
+	private function date aleatoire (){
+
+		return Carbon::createFromDate('null', rand(1,12),rand(1,28));
+	}
     /**
      * Run the database seeds.
      *
