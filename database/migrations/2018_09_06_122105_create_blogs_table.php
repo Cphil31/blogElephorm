328d6a14e62bb3ee7f5b071f299b4f4ed704cb5c
Cphil31 <cecilephilippe31@gmail.com>
2018-09-07 15:12:49 +0200

    modification migration blog :creer la reference dans la migration

--- database/migrations/2018_09_06_122105_create_blogs_table.php
+++ database/migrations/2018_09_06_122105_create_blogs_table.php
@@ -18,6 +18,7 @@ class CreateBlogsTable extends Migration
             $table->string('titre');
             $table->text('texte');
             $table->integer('categorie_id')->unsigned->nullable();
+            $table->foreign('categorie_id')->references('id')->on('categories');
             $table->timestamps();
         });
     }
