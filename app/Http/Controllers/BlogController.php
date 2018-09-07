3d830ae652b4dc174f5eb462ffdf4e066f981705
Cphil31 <cecilephilippe31@gmail.com>
2018-09-07 12:31:57 +0200

    insertion tables dans le controller

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Blog;
 
 class BlogController extends Controller
 {
     public function index(){
-    	return view ('child');
+    	$blog = DB::table('blogs')->get();
+    	return view ('child',['blog'=> $blog]);
     }
 }
