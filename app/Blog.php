584f6db4b7c4aeec53db62019f7107c3388477ee
Cphil31 <cecilephilippe31@gmail.com>
2018-09-07 14:52:47 +0200

    liaison des tables :ajout de commentaires

--- app/Blog.php
+++ app/Blog.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     protected $fillable=['titre','texte']; 
-
+// Un article auras une seule catégorie
     public function categorie(){
     	return $this->BelongsTo('App/Categorie');
     }
