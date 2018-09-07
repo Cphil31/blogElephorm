fd5b210111b0625515521c03f275e7c94dec2cd4
Cphil31 <cecilephilippe31@gmail.com>
2018-09-07 12:37:43 +0200

    affichage de la table blog

--- resources/views/child.blade.php
+++ resources/views/child.blade.php
@@ -10,4 +10,12 @@
 
 @section('content')
     <div class="titre">Page d'acceuil</div>
+
+    @foreach($blog as $blog)
+
+    <p>{{$blog->id}}</p>
+    <p>{{$blog->titre}}</p>
+    <p>{{$blog->texte}}</p>
+
+    @endforeach
 @endsection
