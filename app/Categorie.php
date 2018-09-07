90a2af5a4e90cf92fc4813b66ed388a71e90d363
Cphil31 <cecilephilippe31@gmail.com>
2018-09-07 14:55:42 +0200

    model catégorie : pas besoin de retenir le timstamps

--- app/Categorie.php
+++ app/Categorie.php
@@ -6,8 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categorie extends Model
 {
+	// je n'ai pas besoin de temps quand je choisi la catégorie
+	public $timestamps = false;
     protected $fillable=['titre_categories'];
-
+// une catégorie auras plusierus articles 
     public function blogposts(){
     	return $this->hasMany('App/Blog');
     }
