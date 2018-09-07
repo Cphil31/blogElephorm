4be015092dcbd67eee833e8d0f9efda17dd528f9
Cphil31 <cecilephilippe31@gmail.com>
2018-09-07 14:50:27 +0200

    liaison des tables :une catégorie auras plusieurs articles

--- app/Blog.php
+++ app/Blog.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Blog extends Model
 {
     protected $fillable=['titre','texte']; 
+
+    public function categorie(){
+    	return $this->BelongsTo('App/Categorie');
+    }
 }
--- app/Categorie.php
+++ app/Categorie.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Categorie extends Model
 {
     protected $fillable=['titre_categories'];
+
+    public function blogposts(){
+    	return $this->hasMany('App/Blog');
+    }
 }
